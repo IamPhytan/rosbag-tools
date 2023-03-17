aa851bf5a4b6129ccf1f4ebb632fabb5ad04de59
Damien LaRocque <phicoltan@gmail.com>
2023-03-17 16:19:52 -0400

    Minimal CLI

---
+++ rosbag_tools/__init__.py
@@ -0,0 +1,3 @@
+"""A ROS-agnostic toolbox for common rosbag operations"""
+
+__version__ = "0.0.1"
---
+++ rosbag_tools/__main__.py
@@ -0,0 +1,10 @@
+import click
+
+
+@click.group()
+def cli_main():
+    pass
+
+
+if __name__ == "__main__":
+    cli_main()
