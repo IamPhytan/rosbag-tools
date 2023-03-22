b3a7c6494952fae0abc491052fa481037694ece8
Damien LaRocque <phicoltan@gmail.com>
2023-03-22 17:35:57 -0400

    CLI context settings

--- rosbag_tools/__main__.py
+++ rosbag_tools/__main__.py
@@ -1,7 +1,12 @@
 import click
 
+from . import __version__
 
-@click.group()
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(__version__)
 def cli_main():
     pass
 
