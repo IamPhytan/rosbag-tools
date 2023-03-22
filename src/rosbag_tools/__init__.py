330c42e29aedbf653a10296c8da6fab4cc0807bb
Damien LaRocque <phicoltan@gmail.com>
2023-03-22 18:34:37 -0400

    Add topic-compare as a cli command

--- src/rosbag_tools/__init__.py
+++ src/rosbag_tools/__init__.py
@@ -1,3 +1,5 @@
 """A ROS-agnostic toolbox for common rosbag operations"""
 
+from .topic_compare import topic_compare
+
 __version__ = "0.0.0"
--- src/rosbag_tools/__main__.py
+++ src/rosbag_tools/__main__.py
@@ -1,4 +1,5 @@
 import click
+from . import topic_compare
 
 from . import __version__
 
@@ -8,7 +9,11 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(__version__)
 def cli_main():
+    """A ROS-agnostic toolbox for common rosbag operations"""
     pass
+
+
+cli_main.add_command(topic_compare)
 
 
 if __name__ == "__main__":
